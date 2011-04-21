478769067a2f685600c35194645bebd94970942e
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-04-21 15:54:15 -0400

    ENH: Remove m_Id from NounToBeDefined
    Change-Id: I96560c9c05c6c2da718158ffdf0fb1b8908a095d

--- Modules/Segmentation/LevelSetsv4/include/itkLevelSetDomainMapImageFilter.h
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetDomainMapImageFilter.h
@@ -91,12 +91,10 @@ class ITK_EXPORT LevelSetDomainMapImageFilter : public ImageToImageFilter<
       {
       NounToBeDefined() {}
 
-      NounToBeDefined( const IdentifierType& id,
-                      const InputImageRegionType& reg,
+      NounToBeDefined( const InputImageRegionType& reg,
                       const InputImagePixelType& iList ) :
-        m_Id( id ), m_Region( reg ), m_List( iList ) {}
+        m_Region( reg ), m_List( iList ) {}
 
-      IdentifierType m_Id;
       InputImageRegionType m_Region;
       InputImagePixelType m_List;
       };
