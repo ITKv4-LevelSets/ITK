684188743b15c3de4010627c6a6b41a489a9cd83
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-05-02 13:46:13 -0400

    ENH: Add 1 test for MalcolmSparseLevelSetBase
    Change-Id: I54749baec2d479e224e664079d716c8c9edd4f39

---
+++ Modules/Segmentation/LevelSetsv4/test/itkMalcolmSparseLevelSetBaseTest.cxx
@@ -0,0 +1,54 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkMalcolmSparseLevelSetBase.h"
+
+int itkMalcolmSparseLevelSetBaseTest( int , char* [] )
+{
+  const unsigned int Dimension = 3;
+  typedef itk::MalcolmSparseLevelSetBase< Dimension > SparseLevelSetType;
+
+  SparseLevelSetType::Pointer phi = SparseLevelSetType::New();
+
+  typedef SparseLevelSetType::SparseImageType SparseImageType;
+
+  SparseImageType::IndexType index;
+  index[0] = 0;
+  index[1] = 0;
+
+  SparseImageType::SizeType size;
+  size[0] = 10;
+  size[1] = 10;
+
+  SparseImageType::RegionType region;
+  region.SetIndex( index );
+  region.SetSize( size );
+
+  SparseLevelSetType::NodeAttributeType value;
+  value.m_Status = -3;
+  value.m_Value = -3.;
+
+  SparseImageType::Pointer image = SparseImageType::New();
+  image->SetRegions( region );
+  image->Allocate();
+  image->FillBuffer( value );
+
+  phi->SetImage( image );
+
+  return EXIT_SUCCESS;
+}
