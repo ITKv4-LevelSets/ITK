fcccabf6b7c16a0d6694c8436566c8c6d5917848
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-05-03 16:18:52 -0400

    WIP: Starts implementing the update of the active layer
    Change-Id: I24370709ad92d7faaa8fb73d83fbb016cfcd32c8

--- Modules/Segmentation/LevelSetsv4/include/itkLevelSetSparseEvolutionBase.h
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetSparseEvolutionBase.h
@@ -181,112 +181,104 @@ protected:
 
   void ComputeIteration()
     {
-    DomainIteratorType map_it = m_DomainMapFilter->m_LevelSetMap.begin();
-    DomainIteratorType map_end = m_DomainMapFilter->m_LevelSetMap.end();
+    // first let's get the 0-list
+    LevelSetContainerIteratorType ls_it = m_LevelSetContainer->Begin();
+    LevelSetContainerIteratorType ls_end = m_LevelSetContainer->End();
 
-    std::cout << "Begin iteration" << std::endl;
-
-    while( map_it != map_end )
+    while( ls_it != ls_end )
       {
-//       std::cout << map_it->second.m_Region << std::endl;
+      NodeListType* list_of_nodes = ( ls_it->second )->GetListNode( 0 );
 
-      InputImageIteratorType it( m_InputImage, map_it->second.m_Region );
-      it.GoToBegin();
+      NodeListIterator node_it = list_of_nodes->begin();
+      NodeListIterator node_end = list_of_nodes->end();
 
-      while( !it.IsAtEnd() )
+      LevelSetPointer LevelSetUpdate = m_UpdateBuffer->GetLevelSet( ls_it->first );
+
+      while( node_it != node_end )
         {
-//         std::cout << it.GetIndex() << std::endl;
-        IdListType lout = map_it->second.m_List;
+        idx = ( *node_it )->first;
 
-        if( lout.empty() )
-          {
-          itkGenericExceptionMacro( <<"No level set exists at voxel" );
-          }
+        // get the contribution from all terms at idx
+        InputPixelRealType temp_update =
+            m_EquationContainer->GetEquation( ls_it->first )->Evaluate( idx );
 
-        for( IdListIterator lIt = lout.begin(); lIt != lout.end(); ++lIt )
-          {
-//           std::cout << *lIt << " ";
-          LevelSetPointer levelSet = m_LevelSetContainer->GetLevelSet( *lIt - 1);
-//           std::cout << levelSet->Evaluate( it.GetIndex() ) << ' ';
+        levelSetUpdate->GetImage()->SetPixel( idx, temp_update );
 
-          LevelSetPointer levelSetUpdate = m_UpdateBuffer->GetLevelSet( *lIt - 1);
-
-          InputPixelRealType temp_update =
-              m_EquationContainer->GetEquation( *lIt - 1 )->Evaluate( it.GetIndex() );
-
-          levelSetUpdate->GetImage()->SetPixel( it.GetIndex(), temp_update );
-          }
-//         std::cout << std::endl;
-        ++it;
+        ++node_it;
         }
-      ++map_it;
+      ++ls_it;
       }
     }
 
 
+  // ---------------------------------------------------------------------------
   void InitializeIteration()
   {
-    DomainIteratorType map_it = m_DomainMapFilter->m_LevelSetMap.begin();
-    DomainIteratorType map_end = m_DomainMapFilter->m_LevelSetMap.end();
+//    DomainIteratorType map_it = m_DomainMapFilter->m_LevelSetMap.begin();
+//    DomainIteratorType map_end = m_DomainMapFilter->m_LevelSetMap.end();
 
-    std::cout << "Initialize iteration" << std::endl;
+//    std::cout << "Initialize iteration" << std::endl;
 
-    while( map_it != map_end )
-    {
-//       std::cout << map_it->second.m_Region << std::endl;
+//    while( map_it != map_end )
+//    {
+////       std::cout << map_it->second.m_Region << std::endl;
 
-      InputImageIteratorType it( m_InputImage, map_it->second.m_Region );
-      it.GoToBegin();
+//      InputImageIteratorType it( m_InputImage, map_it->second.m_Region );
+//      it.GoToBegin();
 
-      while( !it.IsAtEnd() )
-      {
-//         std::cout << it.GetIndex() << std::endl;
-        IdListType lout = map_it->second.m_List;
+//      while( !it.IsAtEnd() )
+//      {
+////         std::cout << it.GetIndex() << std::endl;
+//        IdListType lout = map_it->second.m_List;
 
-        if( lout.empty() )
-        {
-          itkGenericExceptionMacro( <<"No level set exists at voxel" );
-        }
+//        if( lout.empty() )
+//        {
+//          itkGenericExceptionMacro( <<"No level set exists at voxel" );
+//        }
 
-        for( IdListIterator lIt = lout.begin(); lIt != lout.end(); ++lIt )
-        {
-          m_EquationContainer->GetEquation( *lIt - 1 )->Initialize( it.GetIndex() );
-        }
-        ++it;
-      }
-      ++map_it;
-    }
-    m_EquationContainer->Update();
+//        for( IdListIterator lIt = lout.begin(); lIt != lout.end(); ++lIt )
+//        {
+//          m_EquationContainer->GetEquation( *lIt - 1 )->Initialize( it.GetIndex() );
+//        }
+//        ++it;
+//      }
+//      ++map_it;
+//    }
+//    m_EquationContainer->Update();
   }
 
+  // ---------------------------------------------------------------------------
   void GenerateData()
     {
     m_InputImage = m_EquationContainer->GetInput();
 
     // Get the LevelSetContainer from the EquationContainer
-    m_LevelSetContainer = m_EquationContainer->GetEquation( 0 )->GetTerm( 0 )->GetLevelSetContainer();
+    m_LevelSetContainer =
+        m_EquationContainer->GetEquation( 0 )->GetTerm( 0 )->GetLevelSetContainer();
+
     AllocateUpdateBuffer();
 
     InitializeIteration();
 
-    for( unsigned int iter = 0; iter < m_NumberOfIterations; iter++ )
+    for( unsigned int iter = 0; iter < m_NumberOfIterations; ++iter )
       {
-      m_RMSChangeAccumulator = 0;
+      m_RMSChangeAccumulator = NumericTraits< LevelSetOutputType >::Zero;
 
       // one iteration over all container
       // update each level set based on the different equations provided
       ComputeIteration();
 
-//       ComputeCFL();
-
       ComputeDtForNextIteration();
 
       UpdateLevelSets();
+
       Reinitialize();
+
       UpdateEquations();
       }
     }
 
+  // ---------------------------------------------------------------------------
   void ComputeDtForNextIteration()
     {
     if( !m_UserDefinedDt )
@@ -312,84 +304,204 @@ protected:
       }
 
       std::cout << "Dt = " << m_Dt << std::endl;
-//       m_Dt = 0.08;
+    }
+
+  // ---------------------------------------------------------------------------
+  void UpdateActiveLayers()
+    {
+    LevelSetContainerIteratorType ls_it = m_LevelSetContainer->Begin();
+    LevelSetContainerIteratorType ls_end = m_LevelSetContainer->End();
+
+    // iterate on all level set
+    while( ls_it != ls_end )
+      {
+      // get the active layer (zero level-set)
+      NodeListType* list_of_nodes = ( ls_it->second )->GetListNode( 0 );
+
+      // create a 2 temporary lists to keep track of what is going up, and what
+      // is going down
+      NodeListType up_list, down_list;
+
+      // update the active layer
+      UpdateActiveLayer( list_of_nodes, &up_list, &down_list );
+
+
+      ++ls_it;
+      }
+    }
+
+  void UpdateActiveLayer(NodeListType* iCurrentList,
+                         NodeListType* ioUpList,
+                         NodeListType* ioDownList )
+    {
+    const LevelSetOutputType LowerActiveThreshold = - 0.5 * m_ConstantGradientValue;
+    const LevelSetOutputType UpperActiveThreshold = 0.5 * m_ConstantGradientValue;
+
+    NodeListIterator node_it = iCurrentList->begin();
+    NodeListIterator node_end = iCurrentList->end();
+
+    LevelSetPointer LevelSetUpdate = m_UpdateBuffer->GetLevelSet( ls_it->first );
+
+    NeighborhoodIterator< LevelSetImageType >
+      n_It( m_NeighborList.GetRadius(),
+            ( ls_it->second )->GetImage(), // get the resulting level set image
+            this->GetOutput()->GetRequestedRegion() );
+
+    while( node_it != node_end )
+      {
+      current_index = ( *node_it )->first;
+      current_attributes = ( *node_it )->second;
+
+      n_It.SetLocation( current_index );
+
+      // get the update for the given level set and the given pixel
+      InputPixelRealType temp_update =
+          m_EquationContainer->GetEquation( ls_it->first )->Evaluate( idx );
+
+      // initialize new_value to the current level set value
+      LevelSetOutputType new_value = attributes.m_Value;
+        new_value += m_Dt * temp_update.m_Value;
+
+      if( new_value >= UpperActiveThreshold )
+        {
+        // This index will move UP into a positive (outside) layer.
+        // First check for active layer neighbors moving in the opposite
+        // direction.
+        flag = false;
+
+        for( unsigned int i = 0; i < m_NeighborList.GetSize(); i++ )
+          {
+          neigh_attributes = n_It.GetPixel( m_NeighborList.GetArrayIndex(i) );
+
+          if( neigh_attributes.m_Status == m_StatusChangingDown )
+            {
+            flag = true;
+            break;
+            }
+          }
+        if( flag )
+          {
+          ++node_it;
+          }
+        else
+          {
+          rms_change_accumulator +=
+              vnl_math_sqr( new_value - outputIt.GetCenterPixel() );
+
+          // Search the neighborhood for inside indicies.
+          LevelSetOutputType temp_value = new_value - m_ConstantGradientValue;
+
+          for ( unsigned int i = 0; i < m_NeighborList.GetSize(); ++i )
+            {
+            internal_idx = m_NeighborList.GetArrayIndex(i);
+            neigh_attributes = n_It.GetPixel(idx);
+
+            if ( neigh_attributes.m_Status == 1 )
+              {
+              // Keep the smallest possible value for the new active node.  This
+              // places the new active layer node closest to the zero level-set.
+              LevelSetOutputType output_value = neigh_attributes.m_Value;
+
+              if( ( output_value < LowerActiveThreshold ) ||
+                    ( vnl_math_abs(temp_value) < vnl_math_abs( output_value ) ) )
+                {
+                neigh_attributes.m_Value = temp_value;
+
+                n_It.SetPixel( idx, neigh_attributes, bounds_status );
+                }
+              }
+            }
+
+          current_attributes.m_Status = m_StatusActiveChangingUp;
+          current_attributes.m_Value = new_value;
+
+          NodePairType node( current_index, attributes );
+
+          ioUpList->push_front( node );
+
+          release_node = layerIt.GetPointer();
+          node_it = iCurrentList->erase( node_it );
+          }
+        }
+      ++node_it;
+      }
     }
 
   virtual void UpdateLevelSets()
     {
-    LevelSetContainerIteratorType it1 = m_LevelSetContainer->Begin();
-    LevelSetContainerConstIteratorType it2 = m_UpdateBuffer->Begin();
+//    LevelSetContainerIteratorType it1 = m_LevelSetContainer->Begin();
+//    LevelSetContainerConstIteratorType it2 = m_UpdateBuffer->Begin();
 
-    LevelSetOutputType p;
+//    LevelSetOutputType p;
 
-    while( it1 != m_LevelSetContainer->End() )
-      {
-      LevelSetImagePointer image1 = it1->second->GetImage();
-      LevelSetImagePointer image2 = it2->second->GetImage();
+//    while( it1 != m_LevelSetContainer->End() )
+//      {
+//      LevelSetImagePointer image1 = it1->second->GetImage();
+//      LevelSetImagePointer image2 = it2->second->GetImage();
 
-      LevelSetImageIteratorType It1( image1, image1->GetBufferedRegion() );
-      LevelSetImageIteratorType It2( image2, image2->GetBufferedRegion() );
-      It1.GoToBegin();
-      It2.GoToBegin();
+//      LevelSetImageIteratorType It1( image1, image1->GetBufferedRegion() );
+//      LevelSetImageIteratorType It2( image2, image2->GetBufferedRegion() );
+//      It1.GoToBegin();
+//      It2.GoToBegin();
 
-      while( !It1.IsAtEnd() )
-        {
-        p = m_Dt * It2.Get();
-        It1.Set( It1.Get() + p );
+//      while( !It1.IsAtEnd() )
+//        {
+//        p = m_Dt * It2.Get();
+//        It1.Set( It1.Get() + p );
 
-        m_RMSChangeAccumulator += p*p;
+//        m_RMSChangeAccumulator += p*p;
 
-        ++It1;
-        ++It2;
-        }
+//        ++It1;
+//        ++It2;
+//        }
 
-      ++it1;
-      ++it2;
-      }
+//      ++it1;
+//      ++it2;
+//      }
   }
 
   void UpdateEquations()
     {
-    InitializeIteration();
+//    InitializeIteration();
 //     m_EquationContainer->Update();
     }
 
   void Reinitialize()
   {
-    LevelSetContainerIteratorType it = m_LevelSetContainer->Begin();
+//    LevelSetContainerIteratorType it = m_LevelSetContainer->Begin();
 
-    while( it != m_LevelSetContainer->End() )
-      {
-      LevelSetImagePointer image = it->second->GetImage();
+//    while( it != m_LevelSetContainer->End() )
+//      {
+//      LevelSetImagePointer image = it->second->GetImage();
 
-      ThresholdFilterPointer thresh = ThresholdFilterType::New();
-      thresh->SetLowerThreshold(
-            NumericTraits< LevelSetOutputType >::NonpositiveMin() );
-      thresh->SetUpperThreshold( 0 );
-      thresh->SetInsideValue( 1 );
-      thresh->SetOutsideValue( 0 );
-      thresh->SetInput( image );
-      thresh->Update();
+//      ThresholdFilterPointer thresh = ThresholdFilterType::New();
+//      thresh->SetLowerThreshold(
+//            NumericTraits< LevelSetOutputType >::NonpositiveMin() );
+//      thresh->SetUpperThreshold( 0 );
+//      thresh->SetInsideValue( 1 );
+//      thresh->SetOutsideValue( 0 );
+//      thresh->SetInput( image );
+//      thresh->Update();
 
-      MaurerPointer maurer = MaurerType::New();
-      maurer->SetInput( thresh->GetOutput() );
-      maurer->SetSquaredDistance( false );
-      maurer->SetUseImageSpacing( true );
-      maurer->SetInsideIsPositive( false );
-      maurer->Update();
+//      MaurerPointer maurer = MaurerType::New();
+//      maurer->SetInput( thresh->GetOutput() );
+//      maurer->SetSquaredDistance( false );
+//      maurer->SetUseImageSpacing( true );
+//      maurer->SetInsideIsPositive( false );
+//      maurer->Update();
 
-      LevelSetImageIteratorType It1( image, image->GetBufferedRegion() );
-      LevelSetImageIteratorType It2( maurer->GetOutput(), image->GetBufferedRegion() );
-      It1.GoToBegin();
-      It2.GoToBegin();
-      while( !It1.IsAtEnd() )
-        {
-        It1.Set( It2.Get() );
-        ++It1;
-        ++It2;
-        }
-      ++it;
-      }
+//      LevelSetImageIteratorType It1( image, image->GetBufferedRegion() );
+//      LevelSetImageIteratorType It2( maurer->GetOutput(), image->GetBufferedRegion() );
+//      It1.GoToBegin();
+//      It2.GoToBegin();
+//      while( !It1.IsAtEnd() )
+//        {
+//        It1.Set( It2.Get() );
+//        ++It1;
+//        ++It2;
+//        }
+//      ++it;
+//      }
   }
 
 private:
