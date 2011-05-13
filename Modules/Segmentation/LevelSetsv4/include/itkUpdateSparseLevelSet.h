4bd8507f6594d5a3a817b2a55096e334c1d00cca
Kishore Mosaliganti <kishoreraom@gmail.com>
2011-05-13 14:00:42 -0400

    ENH: Procedure2 UpdateSparseLevelSet
    Change-Id: I6b71c1886ab99890f62cc29142a6a61c6c8f0922

---
+++ Modules/Segmentation/LevelSetsv4/include/itkUpdateSparseLevelSet.h
@@ -0,0 +1,115 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkUpdateSparseLevelSet_h
+#define __itkUpdateSparseLevelSet_h
+
+#include "itkImage.h"
+#include "itkLevelSetImageBase.h"
+#include "itkWhitakerSparseLevelSetBase.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkShapedNeighborhoodIterator.h"
+#include "itkNeighborhoodAlgorithm.h"
+#include <list>
+#include "itkObject.h"
+
+namespace itk
+{
+template< typename TLevelSetValueType >
+class UpdateSparseLevelSet : public Object
+{
+public:
+  typedef UpdateSparseLevelSet  Self;
+  typedef SmartPointer< Self >                        Pointer;
+  typedef SmartPointer< const Self >                  ConstPointer;
+  typedef Object                                      Superclass;
+
+  /** Method for creation through object factory */
+  itkNewMacro( Self );
+
+  /** Run-time type information */
+  itkTypeMacro( UpdateSparseLevelSet, Object );
+
+  typedef TLevelSetValueType                    InputImagePixelType;
+  typedef Image< InputImagePixelType, ImageDimension >
+                                                InputImageType;
+  typedef typename InputImageType::IndexType    InputImageIndexType;
+  typedef typename InputImageType::Pointer      InputImagePointer;
+  typedef typename InputImageType::RegionType   InputImageRegionType;
+  typedef typename NumericTraits< InputImagePixelType >::RealType
+                                                InputPixelRealType;
+
+  itkStaticConstMacro ( ImageDimension, unsigned int,
+                       InputImageType::ImageDimension );
+
+  typedef TLevelSetValueType  LevelSetOutputType;
+
+  typedef SparseLevelSetBase< LevelSetOutputType, ImageDimension >
+                                                       LevelSetType;
+  typedef typename LevelSetType::Pointer               LevelSetPointer;
+  typedef typename LevelSetType::InputType             LevelSetInputType;
+
+  typedef typename LevelSetType::SparseImageType       SparseImageType;
+  typedef typename SparseImageType::Pointer            SparseImagePointer;
+
+  typedef typename LevelSetType::NodeAttributeType     LevelSetNodeAttributeType;
+  typedef typename LevelSetType::NodeStatusType        LevelSetNodeStatusType;
+  typedef typename LevelSetType::NodePairType          LevelSetNodePairType;
+  typedef typename LevelSetType::NodeListType          LevelSetNodeListType;
+  typedef typename LevelSetType::NodeListIterator      LevelSetNodeListIterator;
+  typedef typename LevelSetType::NodeListConstIterator LevelSetNodeListConstIterator;
+
+  typedef typename LevelSetType::SparseLayerMapType           SparseLayerMapType;
+  typedef typename LevelSetType::SparseLayerMapIterator       SparseLayerMapIterator;
+  typedef typename LevelSetType::SparseLayerMapConstIterator  SparseLayerMapConstIterator;
+
+  typedef ImageRegionIteratorWithIndex< InputImageType >  InputIteratorType;
+  typedef ImageRegionIteratorWithIndex< SparseImageType > SparseIteratorType;
+  typedef ShapedNeighborhoodIterator< InputImageType >    InputNeighborhoodIteratorType;
+  typedef ShapedNeighborhoodIterator< SparseImageType >   SparseNeighborhoodIteratorType;
+
+  // this is the same as Procedure 2
+  // Input is a update image point m_UpdateImage
+  // Input is also WhitakerSparseLevelSetBasePointer
+  void UpdateZeroLevelSet()
+  {
+  }
+
+  // Set/Get the sparse levet set image
+  itkSetObjectMacro( SparseLevelSet, LevelSetType );
+  itkGetObjectMacro( SparseLevelSet, LevelSetType );
+
+  // Set get the input image
+  itkSetObjectMacro( InputImage, InputImageType );
+  itkGetObjectMacro( InputImage, InputImageType );
+
+protected:
+  UpdateSparseLevelSet() {}
+  ~UpdateSparseLevelSet() {}
+
+  InputImagePointer  m_InputImage;
+  LevelSetPointer    m_SparseLevelSet;
+  SparseImagePointer m_SparseImage;
+
+private:
+  UpdateSparseLevelSet( const Self& );
+  void operator = ( const Self& );
+};
+}
+#endif // __itkUpdateSparseLevelSet_h
