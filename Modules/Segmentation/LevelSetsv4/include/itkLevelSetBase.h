2aa0f15d29a7825df4d17cf1bf3bcd5ba8037660
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-08-05 09:34:34 -0400

    ENH: Move implementation from LevelSetBase.h to LevelSetBase.hxx

--- Modules/Segmentation/LevelSetsv4/include/itkLevelSetBase.h
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetBase.h
@@ -62,171 +62,40 @@ public:
    * separated into. */
   itkGetConstMacro(MaximumNumberOfRegions, RegionType);
 
-  virtual void Initialize()
-    {
-    Superclass::Initialize();
-    }
+  virtual void Initialize();
 
   /** Methods to manage streaming. */
-  virtual void UpdateOutputInformation()
-    {
-    if( this->GetSource() )
-      {
-      this->GetSource()->UpdateOutputInformation();
-      }
+  virtual void UpdateOutputInformation();
 
-    // Now we should know what our largest possible region is. If our
-    // requested region was not set yet, (or has been set to something
-    // invalid - with no data in it ) then set it to the largest
-    // possible region.
-    if ( m_RequestedRegion == -1 && m_RequestedNumberOfRegions == 0 )
-      {
-      this->SetRequestedRegionToLargestPossibleRegion();
-      }
-    }
+  virtual void SetRequestedRegionToLargestPossibleRegion();
 
-  virtual void SetRequestedRegionToLargestPossibleRegion()
-    {
-    m_RequestedNumberOfRegions     = 1;
-    m_RequestedRegion           = 0;
-    }
+  virtual void CopyInformation(const DataObject *data);
 
-  virtual void CopyInformation(const DataObject *data)
-    {
-    const LevelSetBase *LevelSet = NULL;
+  virtual void Graft(const DataObject *data);
 
-    try
-      {
-      LevelSet = dynamic_cast< const LevelSetBase * >( data );
-      }
-    catch ( ... )
-      {
-      // pointer could not be cast back down
-      itkExceptionMacro( << "itk::LevelSetBase::CopyInformation() cannot cast "
-                         << typeid( data ).name() << " to "
-                         << typeid( LevelSetBase * ).name() );
-      }
+  virtual bool RequestedRegionIsOutsideOfTheBufferedRegion();
 
-    if ( !LevelSet )
-      {
-      // pointer could not be cast back down
-      itkExceptionMacro( << "itk::LevelSetBase::CopyInformation() cannot cast "
-                         << typeid( data ).name() << " to "
-                         << typeid( LevelSetBase * ).name() );
-      }
-
-    m_MaximumNumberOfRegions = LevelSet->GetMaximumNumberOfRegions();
-
-    m_NumberOfRegions = LevelSet->m_NumberOfRegions;
-    m_RequestedNumberOfRegions = LevelSet->m_RequestedNumberOfRegions;
-    m_BufferedRegion  = LevelSet->m_BufferedRegion;
-    m_RequestedRegion = LevelSet->m_RequestedRegion;
-    }
-
-  virtual void Graft(const DataObject *data)
-    {
-    // Copy Meta Data
-    this->CopyInformation(data);
-
-    const Self *LevelSet = NULL;
-
-    try
-      {
-      LevelSet = dynamic_cast< const Self * >( data );
-      }
-    catch ( ... )
-      {
-      // pointer could not be cast back down
-      itkExceptionMacro( << "itk::LevelSetBase::CopyInformation() cannot cast "
-                         << typeid( data ).name() << " to "
-                         << typeid( Self * ).name() );
-      }
-
-    if ( !LevelSet )
-      {
-      // pointer could not be cast back down
-      itkExceptionMacro( << "itk::LevelSetBase::CopyInformation() cannot cast "
-                         << typeid( data ).name() << " to "
-                         << typeid( Self * ).name() );
-      }
-    }
-
-  virtual bool RequestedRegionIsOutsideOfTheBufferedRegion()
-    {
-    if ( m_RequestedRegion != m_BufferedRegion
-         || m_RequestedNumberOfRegions != m_NumberOfRegions )
-      {
-      return true;
-      }
-
-    return false;
-    }
-
-  virtual bool VerifyRequestedRegion()
-    {
-    bool retval = true;
-
-    // Are we asking for more regions than we can get?
-    if ( m_RequestedNumberOfRegions > m_MaximumNumberOfRegions )
-      {
-      itkExceptionMacro(<< "Cannot break object into "
-                        << m_RequestedNumberOfRegions << ". The limit is "
-                        << m_MaximumNumberOfRegions);
-      }
-
-    if ( m_RequestedRegion >= m_RequestedNumberOfRegions
-         || m_RequestedRegion < 0 )
-      {
-      itkExceptionMacro(<< "Invalid update region " << m_RequestedRegion
-                        << ". Must be between 0 and "
-                        << m_RequestedNumberOfRegions - 1);
-      }
-
-    return retval;
-    }
+  virtual bool VerifyRequestedRegion();
 
   /** Set the requested region from this data object to match the requested
    * region of the data object passed in as a parameter.  This method
    * implements the API from DataObject. The data object parameter must be
    * castable to a PointSet. */
-  virtual void SetRequestedRegion(DataObject *data)
-    {
-    Self *LevelSet = dynamic_cast< Self * >( data );
-
-    if ( LevelSet )
-      {
-      // only copy the RequestedRegion if the parameter is another PointSet
-      m_RequestedRegion = LevelSet->m_RequestedRegion;
-      m_RequestedNumberOfRegions = LevelSet->m_RequestedNumberOfRegions;
-      }
-    }
+  virtual void SetRequestedRegion(DataObject *data);
 
   /** Set/Get the Requested region */
-  virtual void SetRequestedRegion(const RegionType & region)
-    {
-    if ( m_RequestedRegion != region )
-      {
-      m_RequestedRegion = region;
-      }
-    }
+  virtual void SetRequestedRegion(const RegionType & region);
 
   itkGetConstMacro(RequestedRegion, RegionType);
 
   /** Set/Get the Buffered region */
-  virtual void SetBufferedRegion(const RegionType & region)
-    {
-    if ( m_BufferedRegion != region )
-      {
-      m_BufferedRegion = region;
-      this->Modified();
-      }
-    }
+  virtual void SetBufferedRegion(const RegionType & region);
 
   itkGetConstMacro(BufferedRegion, RegionType);
 
 
 protected:
-  LevelSetBase() : Superclass() {}
+  LevelSetBase();
   virtual ~LevelSetBase() {}
 
   // If the RegionType is ITK_UNSTRUCTURED_REGION, then the following
@@ -252,4 +121,5 @@ private:
 };
 }
 
+#include "itkLevelSetBase.hxx"
 #endif // __itkLevelSetBase_h
