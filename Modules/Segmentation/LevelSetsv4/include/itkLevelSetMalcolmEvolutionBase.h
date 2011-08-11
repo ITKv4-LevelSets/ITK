9ef65f66ca93063bb23708ffe83b0879a89faf1a
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-08-11 09:27:50 -0400

    ENH: Add stopping criterion in Malcolm

--- Modules/Segmentation/LevelSetsv4/include/itkLevelSetMalcolmEvolutionBase.h
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetMalcolmEvolutionBase.h
@@ -25,6 +25,7 @@
 #include "itkImageRegionIteratorWithIndex.h"
 #include "itkLevelSetDomainMapImageFilter.h"
 #include "itkUpdateMalcolmSparseLevelSet.h"
+#include "itkLevelSetEvolutionStoppingCriterionBase.h"
 #include <list>
 #include "itkObject.h"
 #include "itkImageFileWriter.h"
@@ -111,6 +112,10 @@ public:
 
   typedef typename UpdateLevelSetFilterType::Pointer UpdateLevelSetFilterPointer;
 
+  typedef LevelSetEvolutionStoppingCriterionBase< LevelSetContainerType >
+                                                  StoppingCriterionType;
+  typedef typename StoppingCriterionType::Pointer StoppingCriterionPointer;
+
   itkSetObjectMacro( LevelSetContainer, LevelSetContainerType );
   itkGetObjectMacro( LevelSetContainer, LevelSetContainerType );
 
@@ -177,16 +182,16 @@ public:
   itkSetObjectMacro( EquationContainer, EquationContainerType );
   itkGetObjectMacro( EquationContainer, EquationContainerType );
 
-  // set the number of iterations
-  itkSetMacro( NumberOfIterations, unsigned int );
-  itkGetMacro( NumberOfIterations, unsigned int );
+  /** \brief Set/Get the Stopping Criterion */
+  itkGetObjectMacro( StoppingCriterion, StoppingCriterionType );
+  itkSetObjectMacro( StoppingCriterion, StoppingCriterionType );
 
   // set the domain map image filter
   itkSetObjectMacro( DomainMapFilter, DomainMapImageFilterType );
   itkGetObjectMacro( DomainMapFilter, DomainMapImageFilterType );
 
 protected:
-  LevelSetMalcolmEvolutionBase() : m_NumberOfIterations( 0 ), m_NumberOfLevelSets( 0 ),
+  LevelSetMalcolmEvolutionBase() : m_StoppingCriterion( NULL ), m_NumberOfLevelSets( 0 ),
     m_InputImage( NULL ), m_EquationContainer( NULL ), m_LevelSetContainer( NULL ),
     m_DomainMapFilter( NULL ), m_Alpha( 0.9 ),
     m_Dt( 1. ), m_RMSChangeAccumulator( -1. ), m_UserDefinedDt( true )
@@ -195,7 +200,8 @@ protected:
   ~LevelSetMalcolmEvolutionBase()
   {}
 
-  unsigned int                m_NumberOfIterations;
+  StoppingCriterionPointer  m_StoppingCriterion;
+
   /// \todo is it useful?
   unsigned int                m_NumberOfLevelSets;
   InputImagePointer           m_InputImage;
@@ -220,7 +226,12 @@ protected:
 
     InitializeIteration();
 
-    for( unsigned int iter = 0; iter < m_NumberOfIterations; iter++ )
+    typename StoppingCriterionType::IterationIdType iter = 0;
+    m_StoppingCriterion->SetCurrentIteration( iter );
+    m_StoppingCriterion->SetLevelSetContainer( m_LevelSetContainer );
+
+//    for( unsigned int iter = 0; iter < m_NumberOfIterations; iter++ )
+    while( !m_StoppingCriterion->IsSatisfied() )
       {
       std::cout <<"Iteration " <<iter << std::endl;
       m_RMSChangeAccumulator = NumericTraits< LevelSetOutputRealType >::Zero;
@@ -260,6 +271,10 @@ protected:
 //         ++it;
 //         }
 
+      ++iter;
+
+      m_StoppingCriterion->SetRMSChangeAccumulator( m_RMSChangeAccumulator );
+      m_StoppingCriterion->SetCurrentIteration( iter );
       this->InvokeEvent( IterationEvent() );
       }
     }
