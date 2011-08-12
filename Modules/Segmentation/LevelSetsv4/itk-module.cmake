6d7964cff905354f40881997a8ea2eb789aeed6d
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-08-12 17:19:24 -0400

    ENH: Minimize module dependency
    Change-Id: I2f94f1c751cba1e57000b1624850f517ac27cff6

--- Modules/Segmentation/LevelSetsv4/itk-module.cmake
+++ Modules/Segmentation/LevelSetsv4/itk-module.cmake
@@ -9,9 +9,7 @@ the segmenting contour.")
 itk_module(ITKLevelSetsv4
   DEPENDS
     ITKCommon
-    ITKStatistics
     ITKLabelMap
-    ITKThresholding # itkBinaryThresholdImageFilter.h
     ITKDistanceMap
   TEST_DEPENDS
     ITKTestKernel
