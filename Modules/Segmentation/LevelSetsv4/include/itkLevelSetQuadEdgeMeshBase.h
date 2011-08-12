46cb201ac790247be0e4689183a8fa600b4975f4
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-08-12 16:19:21 -0400

    ENH: basic implementation of itk::LevelSetQuadEdgeMeshBase
    Change-Id: I4b7a4d31041ef705d85500e76b3735b6c97914ca

--- Modules/Segmentation/LevelSetsv4/include/itkLevelSetQuadEdgeMeshBase.h
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetQuadEdgeMeshBase.h
@@ -23,6 +23,13 @@
 
 namespace itk
 {
+/**
+ *  \class LevelSetQuadEdgeMeshBase
+ *  \brief Base class for the surface mesh representation of a level-set function
+ *
+ *  \tparam TMesh Input mesh type of the level set function
+ *  \todo Think about using image iterators instead of GetPixel()
+ */
 template< class TMesh >
 class LevelSetQuadEdgeMeshBase :
     public LevelSetBase<
@@ -49,52 +56,151 @@ public:
   /** Run-time type information */
   itkTypeMacro ( LevelSetQuadEdgeMeshBase, LevelSetBase );
 
-  typedef typename Superclass::InputType    InputType;
-  typedef typename Superclass::OutputType   OutputType;
-  typedef typename Superclass::GradientType GradientType;
-  typedef typename Superclass::HessianType  HessianType;
+  typedef typename Superclass::InputType      InputType;
+  typedef typename Superclass::OutputType     OutputType;
+  typedef typename Superclass::OutputRealType OutputRealType;
+  typedef typename Superclass::GradientType   GradientType;
+  typedef typename Superclass::HessianType    HessianType;
 
   itkSetObjectMacro( Mesh, MeshType );
   itkGetObjectMacro( Mesh, MeshType );
 
-  OutputType Evaluate( const InputType& iP ) const
+  /** Returns the value of the level set function at a given location iP */
+  virtual OutputType Evaluate( const InputType& iP ) const
     {
     OutputType oValue = 0.;
     m_Mesh->GetPointData( iP, &oValue );
     return oValue;
     }
 
-  GradientType EvaluateGradient( const InputType& iP ) const
+  /** Returns the image gradient of the level set function at a given location iP
+   * \todo to be implemented */
+  virtual GradientType EvaluateGradient( const InputType& iP ) const
     {
+    itkWarningMacro( <<"to be implemented" );
     return GradientType();
     }
 
-  HessianType EvaluateHessian( const InputType& iP ) const
+  /** Returns the image hessian of the level set function at a given location iP
+   * \todo to be implemented */
+  virtual HessianType EvaluateHessian( const InputType& iP ) const
     {
+    itkWarningMacro( <<"to be implemented" );
     return HessianType();
     }
 
-  struct LevelSetDataType
+  /** Returns the value of the level set function at a given location iP
+   * as part of the LevelSetDataType*/
+  virtual void Evaluate( const InputType& iP, LevelSetDataType& ioData ) const
     {
-    OutputType Value;
-    GradientType Gradient;
-    HessianType Hessian;
-    };
-
-  LevelSetDataType* GetAllData( const InputType& iP ) const
-    {
-    LevelSetDataType* oData = new LevelSetDataType;
-    oData->Value = m_Mesh->GetPixel( iP );
-
-    return oData;
+    // if it has not already been computed before
+    if( !ioData.Value.first )
+      {
+      ioData.Value.first = true;
+      m_Mesh->GetPointData( iP, &( ioData.Value.second ) )
+      }
     }
+
+  /** Returns the gradient of the level set function at a given location iP
+   * as part of the LevelSetDataType
+   * \todo to be implemented */
+  virtual void EvaluateGradient( const InputType& iP, LevelSetDataType& ioData ) const
+    {
+    // if it has not already been computed before
+    if( !ioData.Gradient.first )
+      {
+      ioData.Gradient.second = true;
+
+      // compute the gradient
+      ///\todo implement the computation of the gradient
+      }
+    }
+
+  /** Returns the Hessian of the level set function at a given location iP
+   * as part of the LevelSetDataType
+   * \todo to be implemented */
+  virtual void EvaluateHessian( const InputType& iP, LevelSetDataType& ioData ) const
+    {
+    if( !ioData.Hessian.first )
+      {
+      ioData.Hessian.first = true;
+
+      // compute the hessian
+      ///\todo implement the computation of the hessian
+      }
+    }
+
+  /** Initial the level set pointer */
+  virtual void Initialize()
+    {
+    Superclass::Initialize();
+
+    m_Image = NULL;
+    }
+
+  /** Copy level set information from data object */
+  virtual void CopyInformation(const DataObject *data)
+    {
+    Superclass::CopyInformation( data );
+
+    const Self *LevelSet = NULL;
+
+    try
+      {
+      LevelSet = dynamic_cast< const Self * >( data );
+      }
+    catch ( ... )
+      {
+      // LevelSet could not be cast back down
+      itkExceptionMacro( << "itk::LevelSetQuadEdgeMeshBase::CopyInformation() cannot cast "
+                         << typeid( data ).name() << " to "
+                         << typeid( Self * ).name() );
+      }
+
+    if ( !LevelSet )
+      {
+      // pointer could not be cast back down
+      itkExceptionMacro( << "itk::LevelSetQuadEdgeMeshBase::CopyInformation() cannot cast "
+                         << typeid( data ).name() << " to "
+                         << typeid( Self * ).name() );
+      }
+    }
+
+  /** Graft data object as level set object */
+  virtual void Graft( const DataObject* data )
+    {
+    Superclass::Graft( data );
+    const Self *LevelSet = NULL;
+
+    try
+      {
+      LevelSet = dynamic_cast< const Self* >( data );
+      }
+    catch( ... )
+      {
+      // mesh could not be cast back down
+      itkExceptionMacro( << "itk::LevelSetQuadEdgeMeshBase::CopyInformation() cannot cast "
+                         << typeid( data ).name() << " to "
+                         << typeid( Self * ).name() );
+      }
+
+    if ( !LevelSet )
+      {
+      // pointer could not be cast back down
+      itkExceptionMacro( << "itk::LevelSetQuadEdgeMeshBase::CopyInformation() cannot cast "
+                         << typeid( data ).name() << " to "
+                         << typeid( Self * ).name() );
+      }
+
+    this->m_Mesh = LevelSet->m_Mesh;
+    }
+
 
 protected:
-  LevelSetQuadEdgeMeshBase()
+  LevelSetQuadEdgeMeshBase() : Superclass(), m_Mesh( NULL )
     {
-    m_Mesh = MeshType::New();
     }
-  ~LevelSetQuadEdgeMeshBase() {}
+  virtual ~LevelSetQuadEdgeMeshBase() {}
 
   MeshPointer m_Mesh;
 
